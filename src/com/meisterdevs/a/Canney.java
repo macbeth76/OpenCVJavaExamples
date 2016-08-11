8df8a414acd0faadc8907b8e17e8c103028d4020
Andrew Burger <andrew.k.burger@gmail.com>
2016-08-11 18:33:47 -0400

    add package a that detects red rings

--- src/com/meisterdevs/a/Canney.java
+++ src/com/meisterdevs/a/Canney.java
@@ -1,4 +1,4 @@
-package com.meisterdevs;
+package com.meisterdevs.a;
 
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
--- src/com/meisterdevs/a/ball.java
+++ src/com/meisterdevs/a/ball.java
@@ -1,4 +1,4 @@
-package com.meisterdevs;
+package com.meisterdevs.a;
 
 import java.util.ArrayList;
 import java.util.List;
